5baeaf1e248472178f04a8d3c57ce4d214b34554
Nick Stocchero <nick123pig@gmail.com>
2022-09-16 09:20:26 -0500

    stable version

--- devops/main.tf
+++ devops/main.tf
@@ -10,11 +10,43 @@ provider "aws" {
   region = "us-east-1"
 }
 
+<<<<<<< Updated upstream
 resource "aws_iam_user" "sample_terraform_user" {
   name = "sample-terraform-user"
 }
 
+=======
+>>>>>>> Stashed changes
 resource "aws_s3_bucket" "tf-state" {
   bucket = "constable-sample-terraform-state"
   acl    = "private"
+}
+
+resource "aws_s3_bucket" "financial_data" {
+  bucket = "sensitive-financial-data"
+  acl    = "private"
+}
+
+resource "aws_iam_user" "cfo" {
+  name = "chief_financial_officer"
+}
+
+resource "aws_iam_user_policy" "cfo" {
+  name = "cfo"
+  user = aws_iam_user.cfo.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
